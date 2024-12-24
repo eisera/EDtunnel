62928c87215c6dfa8a607f25858bb0ea9b90b893
eisera <97727575+eisera@users.noreply.github.com>
2024-12-24 22:08:04 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '242b47eb-e21c-4c49-8a8b-62f8ae6337cb';
 
 const proxyIPs = ['cdn.xn--b6gac.eu.org:443', 'cdn-all.xn--b6gac.eu.org:443'];
 
